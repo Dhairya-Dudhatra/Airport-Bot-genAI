5188a47fa59c71dda34e0722ab370d7521a4f401
stackgen-automation <automation@stackgen.com>
2025-07-21 13:17:36 -0700

    [skip ci] This PR is created by StackGen to validate connection.

--- cdk-typescript/bin/cdk-typescript.ts
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { CdkTypescriptStack } from '../lib/cdk-typescript-stack';
-
-const app = new cdk.App();
-new CdkTypescriptStack(app, 'Airport-Bot-Stack', {
-  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-});
