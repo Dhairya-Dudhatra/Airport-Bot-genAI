85ae8b73570437ed8761e1c53e53ecf28b24acac
stackgen-automation <automation@stackgen.com>
2025-07-21 13:28:34 -0700

    Commit by Dhairya Anilbhai Dudhatra

---
+++ terraform/main.tf
@@ -0,0 +1,13 @@
+module "stackgen_7c3760c0-ef69-435b-9318-0805105ebffb" {
+  source                = "./modules/aws_iam_role"
+  assume_role_policy    = "{}"
+  description           = null
+  force_detach_policies = true
+  inline_policy         = []
+  max_session_duration  = null
+  name                  = null
+  path                  = null
+  permissions_boundary  = null
+  tags                  = {}
+}
+
