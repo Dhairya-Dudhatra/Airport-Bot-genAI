b30ec9c8331344a2c8967ae27ef99c044c973f11
stackgen-automation <automation@stackgen.com>
2025-07-21 12:59:32 -0700

    Commit by Dhairya Anilbhai Dudhatra

---
+++ terraform/outputs.tf
@@ -0,0 +1,10 @@
+output "aws_iam_role_stackgen_7c3760c0-ef69-435b-9318-0805105ebffb_arn" {
+  value     = module.stackgen_7c3760c0-ef69-435b-9318-0805105ebffb.arn
+  sensitive = false
+}
+
+output "aws_iam_role_stackgen_7c3760c0-ef69-435b-9318-0805105ebffb_name" {
+  value     = module.stackgen_7c3760c0-ef69-435b-9318-0805105ebffb.name
+  sensitive = false
+}
+
