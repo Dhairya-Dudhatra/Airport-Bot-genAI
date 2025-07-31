523d7e515f36d58a73febddc0b293cc36e57c77d
stackgen-automation <automation@stackgen.com>
2025-07-31 13:25:27 -0700

    Commit by Dhairya Anilbhai Dudhatra

---
+++ terraform/outputs.tf
@@ -0,0 +1,10 @@
+output "aws_subnet_stackgen_a3e36ace-ba90-454c-beed-54dd31dd1786_arn" {
+  value     = module.stackgen_a3e36ace-ba90-454c-beed-54dd31dd1786.arn
+  sensitive = false
+}
+
+output "aws_subnet_stackgen_a3e36ace-ba90-454c-beed-54dd31dd1786_id" {
+  value     = module.stackgen_a3e36ace-ba90-454c-beed-54dd31dd1786.id
+  sensitive = false
+}
+
